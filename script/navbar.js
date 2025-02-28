c141fff61b1a08dd60ff843ccb54593976351237
soponalways <soponalways@gmail.com>
2025-03-01 00:55:39 +0600

    Theme Functionalities done

---
+++ script/navbar.js
@@ -0,0 +1,8 @@
+document.getElementById("theme-toggole").addEventListener('click', function(){
+    const r = Math.floor(Math.random() * 256); 
+    const g = Math.floor(Math.random() * 256); 
+    const b = Math.floor(Math.random() * 256); 
+    
+    const bgColor = "rgb(" +r+" , "+g+" , "+b+")"
+    document.body.style.background = bgColor; 
+})
