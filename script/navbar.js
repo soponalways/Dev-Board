138d4b0e8306851643ff9cb7b24866141322a62d
soponalways <soponalways@gmail.com>
2025-03-02 18:57:46 +0600

    Project Done, the Dev Board

--- script/navbar.js
+++ script/navbar.js
@@ -3,7 +3,7 @@ document.getElementById("theme-toggole").addEventListener('click', function(){
     const g = Math.floor(Math.random() * 256); 
     const b = Math.floor(Math.random() * 256); 
     
-    const bgColor = "rgb(" +r+" , "+g+" , "+b+")"
+    const bgColor = `rgb(${r}, ${g}, ${b})`; 
     document.body.style.background = bgColor; 
     document.getElementById("task-bar").style.borderColor = bgColor; 
 })
